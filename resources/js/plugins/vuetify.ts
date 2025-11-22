4f8994f373093fbfa641df63454f2431c6a7a0ae
Daniel Silva <danifilipesilv@gmail.com>
2025-11-22 14:38:46 +0000

    Fix: Fixed vuetify components not working properly

--- resources/js/plugins/vuetify.ts
+++ resources/js/plugins/vuetify.ts
@@ -1,8 +1,12 @@
 import '@mdi/font/css/materialdesignicons.css';
 import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 
 export const vuetify = createVuetify({
+    components,
+    directives,
     icons: {
         defaultSet: 'mdi',
         aliases,
