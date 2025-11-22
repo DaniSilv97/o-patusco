beaa3f3e564e3bd165046f998f2d9bb04b46d4ac
Daniel Silva <danifilipesilv@gmail.com>
2025-11-22 12:01:31 +0000

    Feat: Created Migrations

---
+++ database/migrations/2025_11_22_115926_create_consultation_requests_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consultation_requests', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->date('date');
+            $table->string('client_note');
+            $table->foreignUuid('consultation_timeframe_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('animal_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consultation_requests');
+    }
+};
---
+++ database/migrations/2025_11_22_115931_create_consultations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consultations', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->dateTime('date');
+            $table->string('veterinarian_note');
+            $table->foreignUuid('consultation_state_id')->constrained()->onDelete('cascade');
+            $table->foreignUuid('consultation_request_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consultations');
+    }
+};
